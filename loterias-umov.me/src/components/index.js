100ec508b77dc517be85f21b6472efb1e3219885
Carol Bezerra <carol.arbe@gmail.com>
2021-05-16 17:35:57 -0300

    complete code

--- loterias-umov.me/src/components/index.js
+++
@@ -1,9 +0,0 @@
-import SenaCards from '../components/SenaCards';
-import QuinaCards from '../components/QuinaCards';
-import FinalPrice from '../components/FinalPrice';
-
-export {
-  SenaCards,
-  QuinaCards,
-  FinalPrice,
-}
